b6bdc844e10beee1d3a6efa45fec348ee99d7b3e
Michael Matich <matich.mike@gmail.com>
2020-04-08 23:24:31 +0000

    play and input with wasd support

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function() {
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -11,6 +14,20 @@ const handleUserInput = (input) => { // Do something based on user input
   if (input ===  '\u0003'){
     process.exit();
   }
+  if (input === 'w') {
+    connection.write("Move: up")
+  }
+  if (input === 's') {
+    connection.write("Move: down")
+  }
+  if (input === 'a') {
+    connection.write("Move: left")
+  }
+  if (input === 'd') {
+    connection.write("Move: right")
+  }
 };
+
+
 
 module.exports = setupInput;
