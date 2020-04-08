9536fa916e850ee3f02756c3406694c7cdf050e4
Michael Matich <matich.mike@gmail.com>
2020-04-08 21:16:00 +0000

    play with ctrl c exit

--- play.js
+++ play.js
@@ -1,3 +1,20 @@
 const connect = require('./client');
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+}
+
+const handleUserInput = (input) => { // Do something based on user input
+  if (input ===  '\u0003'){
+    process.exit();
+  }
+};
+
 console.log('Connecting ...');
+setupInput();
 connect();
