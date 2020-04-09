8fc7aca280c47d599e580957ff6d4fa82ab0175e
Michael Matich <matich.mike@gmail.com>
2020-04-09 00:03:25 +0000

    client and constants

--- client.js
+++ client.js
@@ -1,12 +1,12 @@
 const net = require('net');
-
+const {IP, PORT} = require('./constants');
 /**
  * Establishes connection with the game server
  */
 const connect = function() {
   const conn = net.createConnection({ 
-    host: '135.23.222.148',
-    port: 50541
+    host: IP,
+    port: PORT
   })
   
   conn.on('connect', () => {
